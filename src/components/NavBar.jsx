f8b87837ab930897deab3f36d41b749d3d413160
affirnanda <ahmadnanda402@gmail.com>
2025-08-03 15:44:28 +0700

    pembenaran

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,7 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-<<<<<<< HEAD
             <Link to="/">FILM NANDA</Link>
-=======
-            <Link to="/">Movie App</Link>
->>>>>>> 4807e53aa60031bd7d7497c142c7da05e3f03eb9
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
